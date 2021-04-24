a17ae4723d6309863ad378154340c03b771fed25
k-dot-11 <karnatheveer@gmail.com>
2021-04-24 22:09:25 +0530

    kittenn

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AuthPage from './screens/AuthPage';
-
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
+      <ApplicationProvider {...eva} theme={eva.light}>
+
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={AuthPage} />
       </Stack.Navigator>
     </NavigationContainer>
+    </ApplicationProvider>
   );
 }
 
