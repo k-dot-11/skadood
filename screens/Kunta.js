72607b2ba1265ef5b79a96e4796308a6dd1f0ddf
k-dot-11 <karnatheveer@gmail.com>
2021-04-26 14:38:17 +0530

    kunta commit

---
+++ screens/Kunta.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Kunta = () => {
+    return (
+        <View>
+            <Text>fsa</Text>
+        </View>
+    )
+}
+
+export default Kunta
+
+const styles = StyleSheet.create({})
