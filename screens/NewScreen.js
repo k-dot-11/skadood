91f9f1ccfa08068eb44c00c682ff15d050d44064
k-dot-11 <karnatheveer@gmail.com>
2021-04-26 14:19:10 +0530

    testing commit

---
+++ screens/NewScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const NewScreen = () => {
+    return (
+        <View>
+            <Text>fsafsagsag</Text>
+        </View>
+    )
+}
+
+export default NewScreen
+
+const styles = StyleSheet.create({})
